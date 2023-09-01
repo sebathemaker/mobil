a2ea1c966c7a1e18e7c251acfca480934112c120
Sebastian Parraguez <se.parraguez@duocuc.cl>
2023-08-31 22:43:29 -0400

    creacion registro by sebastian

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular'; // Importa IonicRouteStrategy
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy } // Añade el proveedor de IonicRouteStrategy
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  templateUrl: 'register.page.html',
+  styleUrls: ['register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  username: string = ''; 
+  password: string = ''; 
 
-  constructor() { }
+  
+  registeredUsers: { username: string, password: string }[] = [];
 
-  ngOnInit() {
+  constructor(private router: Router) {}
+
+  registerUser() {
+    if (this.registeredUsers.find(user => user.username === this.username)) {
+      
+      alert('El nombre de usuario ya está registrado.');
+    } else {
+  
+      this.registeredUsers.push({ username: this.username, password: this.password });
+      this.router.navigate(['/login']);
+    }
   }
-
 }

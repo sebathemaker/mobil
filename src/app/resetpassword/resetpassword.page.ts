d9a3b35cfd0a485961538cd49d26f56f473f4175
Sebastian Parraguez <se.parraguez@duocuc.cl>
2023-08-31 22:34:47 -0400

    Creacion vista restablecer codigo by sebastian

--- src/app/resetpassword/resetpassword.page.ts
+++ src/app/resetpassword/resetpassword.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-resetpassword',
-  templateUrl: './resetpassword.page.html',
-  styleUrls: ['./resetpassword.page.scss'],
+  templateUrl: 'resetpassword.page.html',
+  styleUrls: ['resetpassword.page.scss'],
 })
-export class ResetpasswordPage implements OnInit {
+export class ResetpasswordPage {
+  username: string = ''; 
+  newPassword: string = ''; 
+  passwordReset: boolean = false; 
 
-  constructor() { }
-
-  ngOnInit() {
+  registeredUsernames: string[] = ['usuario1', 'usuario2', 'usuario3'];
+  
+  constructor(private alertController: AlertController) {} 
+  
+  requestPasswordReset() {
+    if (this.registeredUsernames.includes(this.username)) {
+      this.passwordReset = true;
+    } else {
+      this.presentErrorAlert(); 
+    }
+  }
+  
+  async presentErrorAlert() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Nombre de usuario no encontrado.',
+      buttons: ['OK'],
+    });
+  
+    await alert.present();
   }
 
+  resetPassword() {
+    console.log('Nueva contraseña:', this.newPassword);
+  }
 }

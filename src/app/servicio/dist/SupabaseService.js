24c46264a0ed37ced39b8a6080690bdac32ed4ca
NatanaelPino <nat.pino@duocuc.cl>
2023-10-25 20:11:05 -0300

    cambios

--- src/app/servicio/dist/SupabaseService.js
+++ src/app/servicio/dist/SupabaseService.js
@@ -14,21 +14,111 @@ var SupabaseService = /** @class */ (function () {
     function SupabaseService(httpClient) {
         this.httpClient = httpClient;
         this.supabaseUrl = 'https://mioynnzefjjpspojuedl.susupapabase.co/rest/v1/';
-        this.supabaseKey = process.env.SUPABASE_KEY;
+        this.supabaseKey = supabase_constants_1.supabaseKey;
     }
-    SupabaseService.prototype.getAll = function () {
-        this.httpClient.get(this.supabaseUrl + 'alumno', {
-            headers: new http_1.HttpHeaders({ apikey: supabase_constants_1.supabaseKey })
+    SupabaseService.prototype.getAllAlumno = function () {
+        return this.httpClient.get(this.supabaseUrl + 'alumno', {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
         });
     };
-    SupabaseService.prototype.post = function () {
-        this.httpClient.post(this.supabaseUrl + 'alumno', {
-            Headers: new http_1.HttpHeaders({ apikey: supabase_constants_1.supabaseKey })
+    SupabaseService.prototype.postAlumno = function () {
+        return this.httpClient.post(this.supabaseUrl + 'alumno', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
         });
     };
-    SupabaseService.prototype.put = function () {
-        this.httpClient.put(this.supabaseUrl + 'alumno', {
-            Headers: new http_1.HttpHeaders({ apikey: supabase_constants_1.supabaseKey })
+    SupabaseService.prototype.putAlumno = function () {
+        return this.httpClient.put(this.supabaseUrl + 'alumno', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.getAllProfesor = function () {
+        return this.httpClient.get(this.supabaseUrl + 'profesor', {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.postProfesor = function () {
+        return this.httpClient.post(this.supabaseUrl + 'profesor', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.putProfesor = function () {
+        return this.httpClient.put(this.supabaseUrl + 'profesor', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.getAllAsistencia = function () {
+        return this.httpClient.get(this.supabaseUrl + 'asistencia', {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.postAsistencia = function () {
+        return this.httpClient.post(this.supabaseUrl + 'asistencia', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.putAsistencia = function () {
+        return this.httpClient.put(this.supabaseUrl + 'asistencia', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.getAllAsistenciaAlumno = function () {
+        return this.httpClient.get(this.supabaseUrl + 'asistenciaalumno', {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.postAsistenciaAlumno = function () {
+        return this.httpClient.post(this.supabaseUrl + 'asistenciaalumno', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.putAsistenciaAlumno = function () {
+        return this.httpClient.put(this.supabaseUrl + 'asistenciaalumno', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.getAllSeccion = function () {
+        return this.httpClient.get(this.supabaseUrl + 'seccion', {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.postSeccion = function () {
+        return this.httpClient.post(this.supabaseUrl + 'seccion', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.putSeccion = function () {
+        return this.httpClient.put(this.supabaseUrl + 'seccion', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.getAllClase = function () {
+        return this.httpClient.get(this.supabaseUrl + 'clase', {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.postClase = function () {
+        return this.httpClient.post(this.supabaseUrl + 'clase', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.putClase = function () {
+        return this.httpClient.put(this.supabaseUrl + 'clase', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.getAllSeccionAlumno = function () {
+        return this.httpClient.get(this.supabaseUrl + 'seccionalumno', {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.postSeccionAlumno = function () {
+        return this.httpClient.post(this.supabaseUrl + 'seccionalumno', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
+        });
+    };
+    SupabaseService.prototype.putSeccionAlumno = function () {
+        return this.httpClient.put(this.supabaseUrl + 'seccionalumno', null, {
+            headers: new http_1.HttpHeaders({ apikey: this.supabaseKey })
         });
     };
     SupabaseService = __decorate([
--- src/app/servicio/supabaseservice.ts
+++ src/app/servicio/supabaseservice.ts
@@ -1,41 +1,141 @@
 import { Injectable } from '@angular/core';
 import { createClient } from '@supabase/supabase-js';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { supabaseKey } from './supabase.constants';
-
 
 @Injectable({
     providedIn: 'root',
 })
 export class SupabaseService {
 
-    const supabaseUrl = 'https://mioynnzefjjpspojuedl.susupapabase.co/rest/v1/';
-    const supabaseKey = process.env.SUPABASE_KEY;
+    private supabaseUrl = 'https://mioynnzefjjpspojuedl.susupapabase.co/rest/v1/';
+    private supabaseKey = supabaseKey;
 
+    constructor(private readonly httpClient: HttpClient) { }
 
-    constructor(
-        private readonly httpClient: HttpClient
-    ) { }
-
-    getAll() {
-        this.httpClient.get(this.supabaseUrl + 'alumno', {
-            headers: new HttpHeaders(
-                { apikey: supabaseKey }
-            )
-        })
-    }
-    post() {
-        this.httpClient.post(this.supabaseUrl + 'alumno', {
-            Headers: new HttpHeaders(
-                { apikey: supabaseKey })
-        })
-    }
-    put() {
-        this.httpClient.put(this.supabaseUrl + 'alumno', {
-            Headers: new HttpHeaders(
-                { apikey: supabaseKey })
-        })
+    getAllAlumno() {
+        return this.httpClient.get(this.supabaseUrl + 'alumno', {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
     }
 
+    postAlumno() {
+        return this.httpClient.post(this.supabaseUrl + 'alumno', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
 
+    putAlumno() {
+        return this.httpClient.put(this.supabaseUrl + 'alumno', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    getAllProfesor() {
+        return this.httpClient.get(this.supabaseUrl + 'profesor', {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    postProfesor() {
+        return this.httpClient.post(this.supabaseUrl + 'profesor', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    putProfesor() {
+        return this.httpClient.put(this.supabaseUrl + 'profesor', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    getAllAsistencia() {
+        return this.httpClient.get(this.supabaseUrl + 'asistencia', {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    postAsistencia() {
+        return this.httpClient.post(this.supabaseUrl + 'asistencia', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    putAsistencia() {
+        return this.httpClient.put(this.supabaseUrl + 'asistencia', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    getAllAsistenciaAlumno() {
+        return this.httpClient.get(this.supabaseUrl + 'asistenciaalumno', {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    postAsistenciaAlumno() {
+        return this.httpClient.post(this.supabaseUrl + 'asistenciaalumno', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    putAsistenciaAlumno() {
+        return this.httpClient.put(this.supabaseUrl + 'asistenciaalumno', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    getAllSeccion() {
+        return this.httpClient.get(this.supabaseUrl + 'seccion', {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    postSeccion() {
+        return this.httpClient.post(this.supabaseUrl + 'seccion', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    putSeccion() {
+        return this.httpClient.put(this.supabaseUrl + 'seccion', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    getAllClase() {
+        return this.httpClient.get(this.supabaseUrl + 'clase', {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    postClase() {
+        return this.httpClient.post(this.supabaseUrl + 'clase', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    putClase() {
+        return this.httpClient.put(this.supabaseUrl + 'clase', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    getAllSeccionAlumno() {
+        return this.httpClient.get(this.supabaseUrl + 'seccionalumno', {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    postSeccionAlumno() {
+        return this.httpClient.post(this.supabaseUrl + 'seccionalumno', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
+
+    putSeccionAlumno() {
+        return this.httpClient.put(this.supabaseUrl + 'seccionalumno', null, {
+            headers: new HttpHeaders({ apikey: this.supabaseKey })
+        });
+    }
 }

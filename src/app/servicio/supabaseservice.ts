320a627a7628c4752ebed8414345b57c66a3b90f
NatanaelPino <nat.pino@duocuc.cl>
2023-10-24 23:33:53 -0300

    this

--- src/app/servicio/supabaseservice.ts
+++ src/app/servicio/supabaseservice.ts
@@ -9,29 +9,29 @@ import { supabaseKey } from './supabase.constants';
 })
 export class SupabaseService {
 
-    const supabaseUrl = 'https://mioynnzefjjpspojuedl.susupapabase.co';
+    const supabaseUrl = 'https://mioynnzefjjpspojuedl.susupapabase.co/rest/v1/';
     const supabaseKey = process.env.SUPABASE_KEY;
-    const supabase = createClient(supabaseUrl, supabaseKey);
+
 
     constructor(
         private readonly httpClient: HttpClient
     ) { }
 
     getAll() {
-        this.httpClient.get('https://mioynnzefjjpspojuedl.supabase.co', {
+        this.httpClient.get(this.supabaseUrl + 'alumno', {
             headers: new HttpHeaders(
                 { apikey: supabaseKey }
             )
         })
     }
     post() {
-        this.httpClient.post('https://mioynnzefjjpspojuedl.supabase.co', {
+        this.httpClient.post(this.supabaseUrl + 'alumno', {
             Headers: new HttpHeaders(
                 { apikey: supabaseKey })
         })
     }
     put() {
-        this.httpClient.put('https://mioynnzefjjpspojuedl.supabase.co', {
+        this.httpClient.put(this.supabaseUrl + 'alumno', {
             Headers: new HttpHeaders(
                 { apikey: supabaseKey })
         })
